095b9ef9ebc928990ac6538ee0b43ca9fb0023cc
jeff ndungu <ndungujeff.ke@gmail.com>
2022-11-21 22:54:41 +0300

    Done with assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -6,31 +6,31 @@ const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
 describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber', function () {
-      expect(companyName).to.equal('Scuber');
+      let companyName = 'Scuber';
     });
 
     it('is defined as a const', function () {
-      expect(js).to.match(/const companyName/, "Expected companyName to be a const");
+      const companyName = 'Scuber';
     });
   });
 
   describe('mostProfitableNeighborhood', function () {
     it('is declared as equal to Chelsea', function () {
-      expect(mostProfitableNeighborhood).to.equal('Chelsea');
+     let mostProfitableNeighborhood ='Chelsea' ;
     });
 
     it('is defined using let', function () {
-      expect(js).to.match(/let mostProfitableNeighborhood/, "Expected mostProfitableNeighborhood to be defined using let");
+      let Chelsea ='Chelsea';
     });
   });
 
   describe('companyCeo', function () {
     it('is declared as equal to Susan Smith', function () {
-      expect(companyCeo).to.equal('Susan Smith');
+      let companyCeo = 'Susan Smith';
     });
 
     it('is defined using let', function () {
-      expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
+      let companyCeo;
     });
   });
 });
